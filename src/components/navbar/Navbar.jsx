9d900f793f5f3c0f1466169173618cfa039f7c08
Your Name <you@example.com>
2024-02-27 14:42:50 +0500

    main page created

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav>
-      
+    <nav className='navbar'>
+      <div className='navbar__left'>
+        <input type="text" className='navbar__search'/>
+        <div>
+            <button>Holatlar</button>
+            <button>Hammasi</button>
+            <button>Bo'sh</button>
+            <button>Bron</button>
+            <button>Band</button>
+            <button>Tozalanmoqda</button>
+        </div>
+      </div>
     </nav>
   )
 }
