f1d3905360339ccefeeeb890e734e7d25e688da1
Your Name <you@example.com>
2024-02-27 13:51:50 +0500

    App changed

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,22 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Sidebar from './components/sidebar/Sidebar'
 import "./components/sidebar/sidebar.scss"
+import Navbar from './components/navbar/Navbar'
+import Main from './pages/Main/Main'
 export default function App() {
 
   return (
     <div className=' app'>
       <div className='app_left'>
         <Sidebar />
-        
       </div>
       <div className='app_right'>
-        {/* <Routes>
-          <Route />
-        </Routes> */}
+        <div className='nav'>
+        <Navbar/>
+        </div>
+        <Routes>
+          <Route path='/' element={<Main/>}/>
+        </Routes>
       </div>
     </div>
   )
