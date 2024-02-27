d0ef8c5dbb643563d1e64ca159b471a35b23a4ce
Your Name <you@example.com>
2024-02-27 16:41:17 +0500

    navbar created

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,21 +1,50 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import "./Navbar.scss";
+import { VscListFilter } from "react-icons/vsc";
+import { MdBorderColor } from "react-icons/md";
+import { IoIosArrowForward } from "react-icons/io";
 const Navbar = () => {
+    const [navbarPosition, setNavbarPosition] = useState(false)
+    const [navbarCategory, setNavbarCategory] = useState(false)
+    const [filterList, setFilterList] = useState(false)
   return (
-    <nav className='navbar'>
-      <div className='navbar__left'>
-        <input type="text" className='navbar__search'/>
-        <div>
-            <button>Holatlar</button>
-            <button>Hammasi</button>
-            <button>Bo'sh</button>
-            <button>Bron</button>
-            <button>Band</button>
-            <button>Tozalanmoqda</button>
+    <nav className="navbar">
+      <div className="navbar__item">
+        <input type="text" className="navbar__search" placeholder="Search..." />
+        <div className="navbar__right">
+          <button className="navbar__order">
+            Zakaz berish <MdBorderColor />
+          </button>
+          <button className="navbar__filter-btn" onClick={()=>setFilterList(prev=> !prev)}>
+            Filter <VscListFilter />
+          </button>
+          <div className={`navbar__filter ${filterList ? "right-0" : "right-[-300px]"}`}>
+            <button className="filter__list-btn" onClick={()=>setNavbarPosition(prev=> !prev)}>
+              Holatlar <IoIosArrowForward className={navbarPosition ? " rotate-[90deg] transition-all" : "transition-all"} />
+            </button>
+            <ul className={`navbar__position ${navbarPosition ? "h-[100%] transition-all" : "h-0 transition-all"}`}>
+              <li className="filter__list-item">Hammasi</li>
+              <li className="filter__list-item">Bo'sh</li>
+              <li className="filter__list-item">Bron</li>
+              <li className="filter__list-item">Band</li>
+              <li className="filter__list-item">Tozalanmoqda</li>
+            </ul>
+            <button className="filter__list-btn" onClick={()=>setNavbarCategory(prev=> !prev)}>
+              Kategoriyalar <IoIosArrowForward className={navbarCategory ? " rotate-[90deg] transition-all" : "transition-all"} />
+            </button>
+            <ul className={`navbar__category ${navbarCategory ? "h-[100%] transition-all" : "h-0 transition-all"}`}>
+              <li className="filter__list-item">Oddiy</li>
+              <li className="filter__list-item">Polu-Lyuks</li>
+              <li className="filter__list-item">Lyuks</li>
+              <li className="filter__list-item">3 kishilik</li>
+              <li className="filter__list-item">2 kishilik</li>
+              <li className="filter__list-item">1 kishilik</li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
