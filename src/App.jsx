5b41214e8e18782c9b1508a22947ef4ee123822e
Your Name <you@example.com>
2024-02-27 13:09:41 +0500

    folders are changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Sidebar from './sidebar/Sidebar'
-import "./sidebar/sidebar.scss"
+import Sidebar from './components/sidebar/Sidebar'
+import "./components/sidebar/sidebar.scss"
 export default function App() {
 
   return (
     <div className=' app'>
       <div className='app_left'>
         <Sidebar />
+        
       </div>
       <div className='app_right'>
-
-        <h1>Lorem ipsum dolor sit amet, consectetur adipisic. Eius repellat quaerat eligendi minus sit error, non nisi porro, numquam suscipit deleniti dignissimos fugiat aut animi odio perferendis, laboriosam quibusdam explicabo!</h1>
-
         {/* <Routes>
           <Route />
         </Routes> */}
