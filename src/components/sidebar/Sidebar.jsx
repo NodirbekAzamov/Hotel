dc27b87e7a2068252077cc8ce4de861b9473d857
nodirbekazamov <u900.nodir@gmail.com>
2024-02-27 14:41:12 +0500

    sidebar style

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,30 +3,27 @@ import { Link } from 'react-router-dom'
 import "./sidebar.scss"
 export default function Sidebar() {
     const [components, setComponents] = useState([
-        { id: 1, path: "/", title: "Bosh Sahifa" },
-        { id: 2, path: "/honalar", title: "Honalar" },
-        { id: 3, path: "/hodimlar", title: "Hodimlar" },
-        { id: 2, path: "/honalar", title: "Xonalar" },
-        { id: 3, path: "/hodimlar", title: "Xodimlar" },
-        { id: 4, path: "/mehmonlar", title: "Mehmonlar" },
-        { id: 5, path: "/tashkilotlar", title: "Tashkilotlat" },
-        { id: 6, path: "/sms", title: "SMS" },
+        { id: 1, active: "", path: "/", title: "Bosh Sahifa" },
+        { id: 2, active: "xonalar", path: "/xonalar", title: "Xonalar" },
+        { id: 3, active: "xodimlar", path: "/xodimlar", title: "Xodimlar" },
+        { id: 4, active: "mehmonlar", path: "/mehmonlar", title: "Mehmonlar" },
+        { id: 5, active: "tashkilotlar", path: "/tashkilotlar", title: "Tashkilotlat" },
+        { id: 6, active: "sms", path: "/sms", title: "sms" },
     ])
     const [activeLink, setActiveLink] = useState(1)
-   const url = window.location.href.split("/").pop()
-    const active_link = () => {
-            if (url == components.path) {
-                setActiveLink(url)
-            }
-    }
+
+    const url = window.location.href.split("/").pop()
+
     return (
         <aside className='sidebar'>
+            <Link>
+            <img src="" alt="logo" /></Link>
             <ul>
                 {
                     components?.map((item, index) => {
                         return <div className='sidebar__links' key={index}>
-                            <Link to={item.path} key={index}>{item.title}</Link>
-                            <div onClick={active_link} className={`${activeLink ? "active_link" : ""} `}></div>
+                            <Link to={item.path} key={index} className={`${url === item.active ? "sidebar_link-active" : ""} sidebar_link `}>{item.title}</Link>
+                            <div className={`${url === item.active ? "active_link" : ""} `}></div>
                         </div>
                     })
                 }
