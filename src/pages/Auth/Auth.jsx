23be90e8cede87e9ea0aab4414963d5a8e10982d
Your Name <you@example.com>
2024-02-28 11:04:46 +0500

    ergfds

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -3,12 +3,7 @@ import "./Auth.scss";
 const Auth = () => {
   return (
     <div className='auth'>
-      <h1 className='auth__title'>Log in</h1>
-      <form className='auth__form'>
-        <input type="text" />
-        <input type="password" />
-        <button className='auth__btn'></button>
-      </form>
+      
     </div>
   )
 }
