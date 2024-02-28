85b4bcdf18c70038bc44de5af75a3d3d0e573294
Your Name <you@example.com>
2024-02-28 11:29:12 +0500

    resolved auth

--- src/store/Auth/AuthStore.js
+++ src/store/Auth/AuthStore.js
@@ -5,8 +5,11 @@ const useAuthStore = create((set) => ({
     status: "",
     login: async (payload) => {
         try {
-            const response = await axiosClient.post("/Admin/login-admin", payload)
-            console.log(response);
+            const response = await axiosClient.post("/Auth/login", {...payload})
+            if(response?.data?.token){
+                localStorage.setItem("token", response?.data?.token)
+            }
+            set({status: response?.status})
         } catch (err) {
             console.log(err);
         }
